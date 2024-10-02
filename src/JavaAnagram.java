18424419fe66edcc0bf873cba0d2926425ba582d
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-10-02 12:04:12 +0200

    add javaAnagram

---
+++ src/JavaAnagram.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class JavaAnagram {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String A = bufferedReader.readLine();
+        char[] array = A.toCharArray();
+        String result = "Yes";
+        for (int i = 0; i < array.length / 2; i++) {
+            if (array[i] != array[array.length - i - 1]) {
+                result = "No";
+            }
+        }
+
+        System.out.println(result);
+    }
+}
+
+
