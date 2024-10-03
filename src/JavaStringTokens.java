2d04180c58e94cfd339deac289bc369d8a8123ec
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-10-03 10:13:45 +0200

    add JavaStringTokens

---
+++ src/JavaStringTokens.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+public class JavaStringTokens {
+
+    public static void main(String[] args) {
+
+        String s = "He is a very very good boy, isn't he?";
+        s = s.trim().toLowerCase().replaceAll("[^a-zA-Z]", " ");
+        List<String> listStr = List.of(s.split(" "));
+        int counter = 0;
+        for (String str : listStr) {
+            if (str.isEmpty()) continue;
+            counter++;
+        }
+        System.out.println(counter);
+        for (String str : listStr) {
+            if (str.isEmpty()) continue;
+            System.out.println(str);
+        }
+    }
+
+}
