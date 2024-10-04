66340449cb14d612e22c3e7f1c82f19a163d157d
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-10-04 11:04:53 +0200

    add JavaRegex

---
+++ src/JavaRegex.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.regex.Pattern;
+
+public class JavaRegex {
+
+    public static void main(String[] args) throws IOException {
+        File file = new File("C:\\Users\\NICOLAS.GAUTIER\\intellij-workspace\\java-hackerrank-preparation\\src\\strforjavaregex.txt");
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        String line;
+        Pattern pattern = Pattern.compile(new MyRegex().getIpAddressRegex(), Pattern.CASE_INSENSITIVE);
+        while ((line = bufferedReader.readLine()) != null) {
+            System.out.println(pattern.matcher(line).find());
+        }
+        bufferedReader.close();
+    }
+
+    static class MyRegex{
+        private String ipAddressRegex = "^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+
+        public String getIpAddressRegex() {
+            return ipAddressRegex;
+        }
+    }
+}
