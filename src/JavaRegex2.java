ceceadc254799cabe139c842669d717583e606e8
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-10-04 14:00:55 +0200

    add JavaRegex2 (my solution)

---
+++ src/JavaRegex2.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class JavaRegex2 {
+
+    public static void main(String[] args) throws IOException {
+        File file = new File("C:\\Users\\NICOLAS.GAUTIER\\intellij-workspace\\java-hackerrank-preparation\\src\\strforjavaregex2.txt");
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        int n = Integer.parseInt(bufferedReader.readLine());
+        String regex = "\\b(?i)(\\w+)\\b(?=.*\\b(?i)\\1\\b)";
+
+        for (int i = 0; i < n; i++) {
+            String line = bufferedReader.readLine();
+            String result = line.replaceAll(regex, "").replaceAll("\\s+", " ").trim();
+            System.out.println(result);
+        }
+        bufferedReader.close();
+    }
+}
