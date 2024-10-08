2d33e534fb376edb2848aa6427e089ce2c2c9909
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-10-08 16:07:53 +0200

    add JavaContentExtractor

---
+++ src/JavaTagContentExtractor.java
@@ -0,0 +1,22 @@
+import java.util.regex.*;
+
+public class JavaTagContentExtractor {
+
+    public static void main(String[] args) {
+            String str = "<SA premium>Imtiaz has a secret crush</SA premium>";
+            boolean found = false;
+
+            Pattern pattern = Pattern.compile("<([^<>]+)>([^<>]+)</\\1>");
+            Matcher matcher = pattern.matcher(str);
+
+            while (matcher.find()) {
+                System.out.println(matcher.group(2));
+                found = true;
+            }
+
+            if (!found) {
+                System.out.println("None");
+            }
+
+    }
+}
