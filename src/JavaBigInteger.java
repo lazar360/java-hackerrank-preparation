eb5fb105c791ac6860aaa9e699286b8f975a7bd9
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-10-10 16:09:29 +0200

    add JavaBigInteger

---
+++ src/JavaBigInteger.java
@@ -0,0 +1,18 @@
+import java.math.BigInteger;
+
+public class JavaBigInteger {
+
+
+    public static void main(String[] args) {
+
+    BigInteger bigInteger1 = BigInteger.valueOf(1234);
+    BigInteger bigInteger2 = BigInteger.valueOf(20);
+
+    System.out.println(bigInteger1.add(bigInteger2));
+    System.out.println(bigInteger1.multiply(bigInteger2));
+
+    }
+
+
+
+}
